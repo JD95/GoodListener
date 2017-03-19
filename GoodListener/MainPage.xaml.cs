40aa5dc8d989fdaf12654c6756cb45e9ee247c12
Jeffrey Dwyer <Jeffrey Dwyer>
2017-03-18 20:13:02 -0700

    First

---
+++ GoodListener/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace GoodListener
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+           
+        }
+
+        private async void getMusic_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation =
+                Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".mp3");
+            picker.FileTypeFilter.Add(".wav");
+            picker.FileTypeFilter.Add(".png");
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                // Application now has read/write access to the picked file
+                var t = new Track(file);
+                t.Tapped += track_Clicked;
+                trackList.Items.Add(t);
+            }
+            else
+            {
+                int c = 5;
+            }
+        }
+
+        public void track_Clicked(object sender, RoutedEventArgs args)
+        {
+            var track = sender as Track;
+            if (track != null)
+            {
+                mainPivot.SelectedIndex = 1;
+                player.Source = MediaSource.CreateFromStorageFile(track.track);
+            }
+            else
+            {
+                track.toErrorState();
+            }
+        }
+    }
+}
